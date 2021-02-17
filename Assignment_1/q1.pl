70887a3c2bc1daec6ebb9c7d2125542b22e1e886
Trevor Keegan <tkgn15@live.com>
2021-02-17 16:13:51 -0600

    working on hw1

---
+++ Assignment_1/q1.pl
@@ -0,0 +1,6 @@
+
+here.
+
+jane_has_cat_named_patty :- here.
+john_has_dog_named_curiosity :- jane_has_cat_named_patty.
+curiosity_killed_the_cat :- john_has_dog_named_curiosity.
---
+++ Assignment_1/q2.pl
@@ -0,0 +1,7 @@
+%initial state
+clear(a)
+clear(c)
+clear(table)
+on(a,b)
+on(b,table)
+on(c,table)
