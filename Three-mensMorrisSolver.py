a836e496b8e0766ac43e7f907f38d3923b251f11
Trevor Keegan <keegant@uwplatt.edu>
2021-03-19 11:14:49 -0500

    Update Three-mensMorrisSolver.py

--- Three-mensMorrisSolver.py
+++ Three-mensMorrisSolver.py
@@ -61,6 +61,7 @@ class BoardState:
     possible_spaces = [tuple(map(add, space, delta )) for delta in deltas]
     return [good_space for good_space in possible_spaces if in_board(good_space)]
 
+
   def get_value_at_space(self, space):
     return self._board[space[0]][space[1]]
   
@@ -86,42 +87,77 @@ class BoardState:
     return False
 
   def get_player_spaces(self, player):
+    return self._get_spaces_by_value(player)
+
+  def get_blank_spaces(self):
+    return self._get_spaces_by_value(self.BLANK_SPACE)
+
+  def _get_spaces_by_value(self, value):
     
-    player_occupied_spaces = []
+    spaces_of_interest = []
 
     spaces = range(self.BOARD_SIZE)
     for space in itertools.product(spaces, spaces):
-      if self.get_value_at_space(space) == player:
-        player_occupied_spaces.append(space)
+      if self.get_value_at_space(space) == value:
+        spaces_of_interest.append(space)
 
-    return player_occupied_spaces
+    return spaces_of_interest
 
 
 class SearchNode:
   def __init__(self, board_state):
     self.board_state = board_state
+
+  def is_leaf(self, previous_states):
+    leaf_conditions = [
+      self.board_state in previous_states,
+      self.board_state.check_win()
+    ]
+
+    return any(leaf_conditions)
+
+  def get_heuristic_value(self, player, depth):
+    recorded_win = self.board_state.check_win()
+    if recorded_win:
+      return 1 if player == recorded_win[0] else -1
+    else:
+      return 0
   
-  # If integer n is the number of open spaces on the board, then this function should return a list
-  # of n board states with a player mark for every open space all on different boards such that the
-  # returned board states now have n-1 open spaces.
   def get_neighbors(self, player):
     neighbors = []
     player_spaces = self.board_state.get_player_spaces(player)
-    for player_space in player_spaces:
-      for space in self.board_state.get_neighboring_spaces(player_space):
-        space_val = self.board_state.get_value_at_space(space)
-        if space_val == self.board_state.BLANK_SPACE:
-          new_board = self.board_state.copy()
-          new_board.set_value_at_space(space, player)
-          neighbors.append(new_board)
+    if len(player_spaces) < 3:
+      for space in self.board_state.get_blank_spaces():
+        new_board = self.board_state.copy()
+        new_board.set_value_at_space(space, player)
+        neighbors.append(new_board)
+
+    else:
+      for player_space in player_spaces:
+        for space in self.board_state.get_neighboring_spaces(player_space):
+          space_val = self.board_state.get_value_at_space(space)
+          if space_val == self.board_state.BLANK_SPACE:
+            new_board = self.board_state.copy()
+            new_board.set_value_at_space(space, player)
+            new_board.set_value_at_space(player_space, self.board_state.BLANK_SPACE)
+            neighbors.append(new_board)
 
     return neighbors
     
 
 
+visited_states = []
+def minimax(node, depth, max_player_1):
+  if depth == 0 or node.is_leaf(visited_states):
+    return node.get_heuristic_value(1 if max_player_1 else 2, depth)
+  
+
 bs = BoardState()
-bs.set_value_at_space((0,1),1)
+bs.set_value_at_space((0,0),1)
+bs.set_value_at_space((0,1),2)
+bs.set_value_at_space((0,2),1)
+
 sn = SearchNode(bs)
-neighbors = sn.get_neighbors(1)
-for neighbor in neighbors:
-  print(neighbor)
+print(minimax(sn, 1, True))
+
+
